9aebcc47ed6783326cb49197f9bd90bbd4e3c30d
jongmink0 <kjm010629@naver.com>
2025-07-01 17:06:40 +0900

    feat: react-native-bootsplash로 스플래시 화면 구현 (iOS & Android)

--- front/App.tsx
+++ front/App.tsx
@@ -1,11 +1,16 @@
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import BootSplash from 'react-native-bootsplash';
 import Rootnavigator from './src/navigations/root/Rootnavigator';
 
 function App() {
+  useEffect(() => {
+    BootSplash.hide({fade: true});
+  }, []);
+
   return (
     <NavigationContainer>
       <Rootnavigator />
--- front/ios/YogiitsuApp/AppDelegate.mm
+++ front/ios/YogiitsuApp/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <GoogleMaps/GoogleMaps.h>
+#import "RNBootSplash.h"
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -13,7 +14,14 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  // 먼저 super를 호출해 React Native의 window 및 rootView를 생성
+  BOOL didLaunch = [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+  // 그 다음에 BootSplash를 초기화하여 RootView 위에 덮어놓기
+  [RNBootSplash initWithStoryboard:@"BootSplash"
+                          rootView:self.window.rootViewController.view];
+
+  return didLaunch;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
