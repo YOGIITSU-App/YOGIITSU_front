d42bdf96d08fbce2ec25b9f1fd7dd5e2a0f70b00
jongmink0 <kjm010629@naver.com>
2025-08-09 22:02:08 +0900

    chore: 프로젝트명 변경 및 iOS 위치 권한 설정 수정
    - 앱 프로젝트명 'YogiitsuApp'으로 변경
    - iOS Info.plist에 위치 권한 문구 확인 및 유지
    - Podfile에 react-native-permissions setup_permissions 적용
      - LocationWhenInUse 권한 핸들러 추가

--- front/android/app/src/main/java/com/yogiitsuapp/MainApplication.kt
+++ front/android/app/src/main/java/com/yogiitsuapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.yogiitsuappv802
+package com.yogiitsuapp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- front/ios/YogiitsuApp/AppDelegate.swift
+++ front/ios/YogiitsuApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "YogiitsuAppV802",
+      withModuleName: "YogiitsuApp",
       in: window,
       launchOptions: launchOptions
     )
